c2454c3e0f68dca3d79e7397b8987a6f470c9d52
shix@1000video.com.cn <sx15950512122@>
2019-12-15 10:01:32 +0800

    105

--- src/main/java/tree/Solution105.java
+++ src/main/java/tree/Solution105.java
@@ -1,5 +1,8 @@
 package tree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * created at 2019/12/14
  *
@@ -7,11 +10,43 @@ package tree;
  */
 public class Solution105 {
 
+
+    Map<Integer, Integer> inMap = new HashMap<>();
+    int[] preorder;
+    int[] inorder;
+    int preIndex = 0;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return new TreeNode(-1);
+        this.preorder = preorder;
+        this.inorder = inorder;
+        for (int i = 0; i < inorder.length; i++) {
+            inMap.put(inorder[i], i);
+        }
+
+
+        return helper(0, inorder.length);
     }
 
-    public static void main(String[] args) {
+    private TreeNode helper(int inLeft, int inRight) {
+        if (inLeft == inRight) {
+            return null;
+        }
 
+        int rootVal = preorder[preIndex];
+        TreeNode root = new TreeNode(rootVal);
+        int index = inMap.get(rootVal);
+        preIndex++;
+        root.left = helper(inLeft, index);
+        root.right = helper(index + 1, inRight);
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        int[] preorder = new int[]{3, 9, 11, 20, 15, 7};
+        int[] inorder = new int[]{9, 11, 3, 15, 20, 7};
+        Solution105 solution105 = new Solution105();
+        TreeNode treeNode = solution105.buildTree(preorder, inorder);
+        treeNode.show();
     }
 }
