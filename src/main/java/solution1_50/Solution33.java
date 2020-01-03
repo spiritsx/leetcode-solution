092c20d15de2d6b2c6f2ea1c6b74676e4254f088
shix@1000video.com.cn <shix@1000video.com.cn>
2020-01-03 16:55:20 +0800

    33

---
+++ src/main/java/solution1_50/Solution33.java
@@ -0,0 +1,46 @@
+package solution1_50;
+
+import org.junit.jupiter.api.Assertions;
+
+public class Solution33 {
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //前半部分有序,注意此处用小于等于
+            if (nums[start] <= nums[mid]) {
+                //target在前半部分
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution33 solution33 = new Solution33();
+        int[] nums = new int[]{4,5,6,7,0,1,2};
+        int search = solution33.search(nums, 4);
+        System.out.println(search);
+        Assertions.assertEquals(search, -1);
+    }
+}
